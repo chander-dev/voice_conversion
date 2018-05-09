3ca07a2e63996ae56dc6ef50c9218e7d7d6fc441
chander-dev <ee15btech11006@iith.ac.in>
2018-05-09 09:51:32 +0530

    gan added

---
+++ GAN/vc_demo.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# GAN-based voice cnoversion demo
+
+set -e
+
+experiment_id=$1
+cmu_arctic_dir=$2
+data_dir=./features/ #target directory where the np arrays will be stored  
+run_training=1                          
+
+echo "Experimental id:" $experiment_id
+echo "Data dir:" $data_dir
+echo "CMU Arctic dir:" ${cmu_arctic_dir}
+
+generated_audio_dir=./generated/${experiment_id}
+checkpoints_dir=./checkpoints/${experiment_id}   
+
+# Feature extraction
+python prepare_features_vc.py --max_files=50 ${cmu_arctic_dir} \
+    ksp bdl --dst_dir=${data_dir}
+
+# train_gan.sh args:
+# 1. Hyper param name
+# 2. X: Network inputs
+# 3. Y: Network outputs
+# 4. Where to save checkpoints
+# 5. Generator wamup epoch
+# 6. discriminator_warmup_epoch
+# 7. Total epoch for spoofing model training
+# 8. Total epoch for GAN
+
+# Traing models
+if [ "${run_training}" == 1 ]; then
+    ./train_gan.sh vc ${data_dir}/X ${data_dir}/Y \
+        ${checkpoints_dir} \
+        50 10 50 200 $experiment_id
+fi
+
+### Evaluation ###
+
+# Baseline
+python evaluation_vc.py \
+    ${checkpoints_dir}/baseline/checkpoint_epoch200_Generator.pth \
+    ${data_dir} \
+    ${cmu_arctic_dir}/cmu_us_ksp_arctic/wav \
+    ${generated_audio_dir}/baseline \
+    --diffvc
+
+# GAN
+python evaluation_vc.py \
+    ${checkpoints_dir}/gan/checkpoint_epoch200_Generator.pth \
+    ${data_dir} \
+    ${cmu_arctic_dir}/cmu_us_ksp_arctic/wav \
+    ${generated_audio_dir}/gan \
+    --diffvc
